24cc801fe105fed6fc27dcb64308dd23155efbce
Richard Shea <rshea@thecubagroup.com>
2015-03-25 23:19:41 +1300

    Tidied up form of TIME SERVER

--- time.js
+++ time.js
@@ -4,15 +4,9 @@ var d = new Date();
 
 var strftime = require('strftime')
 
-//var server = net.createServer(function (socket) {
-//    socket.on('data', function(data){
-//        console.log(strftime('%Y-%m-%d %H:%M'))
-//    });
-//})
 var server = net.createServer(function (socket) {
-    var t = strftime('%Y-%m-%d %H:%M')
-    console.log(t);
-    socket.end(t);
+    var t = strftime('%Y-%m-%d %H:%M');
+    socket.end(t + '\n');
 })
 
 server.listen(process.argv[2])
